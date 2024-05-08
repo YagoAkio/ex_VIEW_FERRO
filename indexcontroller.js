958be7502502ab72881f8e0656232a7934427c5f
YagoAkio <yagokuniyoshi1@gmail.com>
2024-05-08 12:24:32 -0300

    Finalizado

--- indexcontroller.js
+++ indexcontroller.js
@@ -28,43 +28,57 @@ const vapp = {
                     option.text = response.data[i].nome;
                     select.appendChild(option);
                 }
+                select.value = '';
             });
         },
         ceps(){
-            axios
-            .get('https://viacep.com.br/ws/' + this.estado + '/' + this.municipio + '/'+ this.referencia +'/json/')
-            .then((response) => { 
-                let table = document.getElementById('tabela');
-                table.innerHTML = `
-                    <tr>
-                        <th>CEP</th>
-                        <th>Logradouro</th>
-                        <th>Complemento</th>
-                        <th>Bairro</th>
-                    </tr>
-                `;
-                for(let i=0;i<response.data.length;i++){
-                    let tr = document.createElement("tr");
-                    tr.innerHTML = `
-                        <td>${response.data[i].cep}</td>
-                        <td>${response.data[i].logradouro}</td>
-                        <td>${response.data[i].complemento}</td>
-                        <td>${response.data[i].bairro}</td>
+            if(this.municipio!='' && this.estado!='' && this.referencia.length>2){
+                axios
+                .get('https://viacep.com.br/ws/' + this.estado + '/' + this.municipio + '/'+ this.referencia +'/json/')
+                .then((response) => { 
+                    let table = document.getElementById('tabela');
+                    table.innerHTML = `
+                        <tr>
+                            <th scope="col">CEP</th>
+                            <th scope="col">Logradouro</th>
+                            <th scope="col">Complemento</th>
+                            <th scope="col">Bairro</th>
+                        </tr>
                     `;
-                    table.appendChild(tr);
-                }
-            });
+                    for(let i=0;i<response.data.length;i++){
+                        let tr = document.createElement("tr");
+                        tr.innerHTML = `
+                            <td>${response.data[i].cep}</td>
+                            <td>${response.data[i].logradouro}</td>
+                            <td>${response.data[i].complemento}</td>
+                            <td>${response.data[i].bairro}</td>
+                        `;
+                        table.appendChild(tr);
+                    }
+                });
+            }
         }
     },
     template:
     `
-        <form>
-            <select @change="add_municipio()" v-model="estado" id="estados_select"></select>
-            <select @change="ceps()" v-model="municipio" id="municipios" ></select>
-            <input @change="ceps()" type="text" v-model="referencia">
+        <form style="
+                    display: flex;
+                    justify-content: space-around;
+                    align-items: center;
+                    width: 100%;
+                    padding: 20px 50px;
+                    "
+        >
+            <select @change="add_municipio()" v-model="estado" id="estados_select" class="form-select" aria-label="Default select example">
+                <option selected>Estados</option>
+            </select>
+            <select @change="ceps()" v-model="municipio" id="municipios" class="form-select" aria-label="Default select example">
+                <option selected>Municipios</option>
+            </select>
+            <input @change="ceps()" v-model="referencia" class="form-control" type="text" placeholder="Referencia" aria-label="default input example">
         </form>
         <br>
-        <table id="tabela">
+        <table class="table" id="tabela">
         </table>
     `
 };
